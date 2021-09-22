9c4fdda065e9964efb48f58038b56895d301e1c5
SteffB23 <sbrookens@hotmail.com>
2021-09-22 01:12:16 -0400

    added prompts after clicking generate pw & added an event listener

--- Develop/script.js
+++ Develop/script.js
@@ -25,8 +25,34 @@ function writePassword() {
   passwordText.value = password;
 
 }
-//Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
+// Prompts that display after clicking generate password:
+function generatePassword() {
+  var passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 12 but less than 128.");
+
+  var num = confirm("Do you want numbers in your password?");
+
+  var abcLower = confirm("Do you want lowercases in your password?");
+
+  var abcUpper = confirm("Do you want uppercases in your password?");
+
+  var sym = confirm("Do you want special characters in your password?");
+
+  // this is a minimum count for numbers, lowerCases, upperCases & specialCharacters
+  var minimumCount = 0;
+}
+
+
+//Added event listener to generate button
+var generateBtn = document.getElementById('generateBtn');
+document.addEventListener('click', generatePassword);
+
+
+
+//window.addEventListener('load', function() {
+//generateNewPassword();
+  //var password = "";
+//  }
+//});
 //document.getElementById("generateBtn").addEventListener("click", function() {
   //document.getElementById("demo").innerHTML = "Hello World";
 //});*/
