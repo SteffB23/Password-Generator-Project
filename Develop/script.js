fbc4c18531f637d126aaf12811fd040359f9a923
SteffB23 <sbrookens@hotmail.com>
2021-09-22 00:30:10 -0400

    variables added to the script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,21 @@
-// Assignment Code
+// Variables
 var generateBtn = document.querySelector("#generate");
+var abcUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var abcUpperArr = abcUpper.split("");
+var abcLower = "abcdefghijklmnopqrstuvwxyz";
+var abcLowerArr = abcLower.split("");
+var num = "0123456789";
+var numArr = num.split("");
+var sym = "!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~";
+var symArr = sym.split("");
+
+//Combined Var abcUpper, abcLower & sym:
+var abcUpperLowerSym = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz" + "!#$%&\()*+,-./:;<=>?@^[\\]^_`{|}~";  
+
+//Immediate message when page loads:
+window.onload = alert("Welcome! Please click 'Generate password' to start!");
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -9,6 +25,10 @@ function writePassword() {
   passwordText.value = password;
 
 }
+//Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
+//document.getElementById("generateBtn").addEventListener("click", function() {
+  //document.getElementById("demo").innerHTML = "Hello World";
+//});*/
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
